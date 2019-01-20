1dab643122824ae38278b9987aa0e6d476f6c0f3
fengxie <fengxie@ebay.com>
2019-01-20 15:47:42 +0800

    add abstract class support

--- starter/stone-dal-starter/src/test/java/stone/dal/adaptor/spring/jdbc/aop/example/repo/PersonRepositoryImpl.java
+++ starter/stone-dal-starter/src/test/java/stone/dal/adaptor/spring/jdbc/aop/example/repo/PersonRepositoryImpl.java
@@ -1,11 +1,18 @@
 package stone.dal.adaptor.spring.jdbc.aop.example.repo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import stone.dal.adaptor.spring.jdbc.api.StJdbcTemplate;
+import stone.dal.adaptor.spring.jdbc.api.meta.SqlCondition;
 import stone.dal.common.models.Person;
 
 public abstract class PersonRepositoryImpl implements PersonRepository {
 
+  @Autowired
+  private StJdbcTemplate jdbcTemplate;
+
   @Override
   public Person findMale() {
+    jdbcTemplate.query(SqlCondition.create(Person.class).eq("name", "Xie"));
     Person person = new Person();
     person.setName("MaleA");
     return person;
