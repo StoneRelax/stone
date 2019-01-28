4024dd2df599a17a166dc81ad63dab0c610185c6
xzang <xzang@ebay.com>
2019-01-28 20:53:45 +0800

    move starter to maven

--- gradle-plugins/stone-do-generator/gen-src/src/main/java/stone/dal/pojo/repo/BannerRepository.java
+++
@@ -1,9 +0,0 @@
-package stone.dal.pojo.repo;
-
-import stone.dal.adaptor.spring.jdbc.api.StJpaRepository;
-import stone.dal.pojo.jpa.Banner;
-
-public interface BannerRepository extends StJpaRepository<Banner, Long> {
-
-
-}
