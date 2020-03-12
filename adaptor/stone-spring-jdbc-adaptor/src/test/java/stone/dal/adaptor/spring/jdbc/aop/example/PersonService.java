420ccbfb5624861f46f7cef46e621eb9aec8c268
xzang <xzang@ebay.com>
2020-03-12 19:04:04 +0800

    revert to swagger 2.0

--- adaptor/stone-spring-jdbc-adaptor/src/test/java/stone/dal/adaptor/spring/jdbc/aop/example/PersonService.java
+++ adaptor/stone-spring-jdbc-adaptor/src/test/java/stone/dal/adaptor/spring/jdbc/aop/example/PersonService.java
@@ -1,8 +1,12 @@
 package stone.dal.adaptor.spring.jdbc.aop.example;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import javax.annotation.PostConstruct;
 
-import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +20,7 @@ import stone.dal.common.models.Person;
 
 @RestController
 @RequestMapping("/person")
+@Api(value = "/person", description = "${className}")
 public class PersonService {
   @Autowired
   private PersonRepository personRepository;
@@ -29,7 +34,7 @@ public class PersonService {
   }
 
   @Transactional
-  @Operation(method = "GET", description = "Query ${doName}")
+  @ApiOperation(httpMethod = "GET", value = "Query ${doName}", notes = "Find ${doName} by uuid")
   public void testTx(Long uuid, String name, boolean testRollback) {
     if (testRollback) {
       personRepository.testTxRollback(uuid, name);
@@ -38,8 +43,12 @@ public class PersonService {
     }
   }
 
+  @ApiParam
   @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-  @Operation(method = "GET", description = "Query ${doName}")
+  @ApiOperation(httpMethod = "GET", value = "Query ${doName}", notes = "Find ${doName} by uuid")
+  @ApiImplicitParams(
+      @ApiImplicitParam(name = "id", value = "${doName} id", required = true, paramType = "path")
+  )
   public @ResponseBody
   Person get(@PathVariable Long id) {
     Person person = new Person();
@@ -49,7 +58,7 @@ public class PersonService {
 
   @Transactional
   @RequestMapping(value = "/", method = RequestMethod.POST)
-  @Operation(method = "POST", description = "Create ${doName}")
+  @ApiOperation(httpMethod = "POST", value = "Create ${doName}", notes = "Create ${doName}")
   public @ResponseBody
   Long create(@RequestBody Person person) {
     return personRepository.create(person);
@@ -57,7 +66,7 @@ public class PersonService {
 
   @Transactional
   @RequestMapping(value = "/", method = RequestMethod.PUT)
-  @Operation(method = "PUT", description = "Update ${doName}")
+  @ApiOperation(httpMethod = "PUT", value = "Update ${doName}", notes = "Update ${doName}")
   public @ResponseBody
   void update(@RequestBody Person person) {
     personRepository.update(person);
@@ -65,7 +74,10 @@ public class PersonService {
 
   @Transactional
   @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-  @Operation(method = "DELETE", description = "Delete ${doName}")
+  @ApiOperation(httpMethod = "DELETE", value = "Delete ${doName}", notes = "Delete ${doName} by uuid")
+  @ApiImplicitParams(
+      @ApiImplicitParam(name = "id", value = "${doName} id", required = true, paramType = "path")
+  )
   public @ResponseBody
   void delete(@PathVariable long id) {
     Person person = new Person();
