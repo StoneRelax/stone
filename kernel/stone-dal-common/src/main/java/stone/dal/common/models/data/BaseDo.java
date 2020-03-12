d1e205c575a55f85b63edff0350d4963ffa56bc8
xzang <xzang@ebay.com>
2020-03-12 19:02:56 +0800

    revert to swagger 2.0

--- kernel/stone-dal-common/src/main/java/stone/dal/common/models/data/BaseDo.java
+++ kernel/stone-dal-common/src/main/java/stone/dal/common/models/data/BaseDo.java
@@ -2,42 +2,45 @@ package stone.dal.common.models.data;
 
 import java.util.HashSet;
 import java.util.Set;
+
 import org.springframework.util.ClassUtils;
-import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
  * @author fengxie
  */
 public class BaseDo {
 
-	@Schema(hidden = true)
+  @ApiModelProperty(hidden = true)
   private States _state;
-	@Schema(hidden = true)
-	private Set<String> _changes = new HashSet<>();
+
+  @ApiModelProperty(hidden = true)
+  private Set<String> _changes = new HashSet<>();
 
   private transient boolean _attached;
-	private Set<String> _lazyLoading = new HashSet<>();
+
+  private Set<String> _lazyLoading = new HashSet<>();
 
   public States get_state() {
-		return _state;
-	}
+    return _state;
+  }
 
   public void set_state(States _state) {
-		this._state = _state;
-	}
+    this._state = _state;
+  }
 
-	public Set<String> get_changes() {
-		return _changes;
-	}
+  public Set<String> get_changes() {
+    return _changes;
+  }
 
-	public void set_changes(Set<String> _changes) {
-		this._changes = _changes;
-	}
+  public void set_changes(Set<String> _changes) {
+    this._changes = _changes;
+  }
 
   public void ackChange(String field) {
     _state = States.Updated;
     _changes.add(field);
-	}
+  }
 
   public void attach() {
     this._attached = true;
@@ -49,23 +52,23 @@ public class BaseDo {
 
   public boolean check_attached() {
     return _attached;
-	}
+  }
 
   public void markDel() {
     _state = States.Deleted;
   }
 
-	public void markLazyLoadedField(String field) {
-		_lazyLoading.add(field);
-	}
+  public void markLazyLoadedField(String field) {
+    _lazyLoading.add(field);
+  }
 
-	public boolean isLoaded(String field) {
-		return _lazyLoading.contains(field);
-	}
+  public boolean isLoaded(String field) {
+    return _lazyLoading.contains(field);
+  }
 
-	protected Class getUserClass(Class clazz) {
-		return ClassUtils.getUserClass(clazz);
-	}
+  protected Class getUserClass(Class clazz) {
+    return ClassUtils.getUserClass(clazz);
+  }
 
   public enum States {
     Updated,
